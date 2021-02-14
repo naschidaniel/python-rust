3cf25b4bfd8a6c190baff82941ff2395c30b85af
Daniel Naschberger <daniel@naschi.at>
2021-02-14 13:30:47 +0100

    chore(lib): init myrustlib

---
+++ pyext-myrustlib/src/lib.rs
@@ -0,0 +1,16 @@
+use cpython::{py_fn, py_module_initializer, PyResult, Python};
+
+fn rs_hello_world(name: &str) -> String {
+    format!("Hello {}! This message comes from Rust.", name)
+}
+
+fn rs_hello_world_py(_: Python, name:&str) -> PyResult<String> {
+    let greeting = rs_hello_world(name);
+    Ok(greeting)
+}
+
+py_module_initializer!(myrustlib, |py, m| {
+    m.add(py, "__doc__", "This module is implemented in Rust.")?;
+    m.add(py, "rs_hello_world", py_fn!(py, rs_hello_world_py(name: &str)))?;
+    Ok(())
+});

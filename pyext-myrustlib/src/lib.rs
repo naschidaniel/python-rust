a61d259ff3e5659e23bfecc3de5e18722e4c28f2
Daniel Naschberger <daniel.naschberger@tbbm.at>
2021-02-15 13:19:14 +0100

    fix(lib): prettier

--- pyext-myrustlib/src/lib.rs
+++ pyext-myrustlib/src/lib.rs
@@ -4,13 +4,17 @@ fn rs_hello_world(name: &str) -> String {
     format!("Hello {}! This message comes from Rust.", name)
 }
 
-fn rs_hello_world_py(_: Python, name:&str) -> PyResult<String> {
+fn rs_hello_world_py(_: Python, name: &str) -> PyResult<String> {
     let greeting = rs_hello_world(name);
     Ok(greeting)
 }
 
 py_module_initializer!(myrustlib, |py, m| {
     m.add(py, "__doc__", "This module is implemented in Rust.")?;
-    m.add(py, "rs_hello_world", py_fn!(py, rs_hello_world_py(name: &str)))?;
+    m.add(
+        py,
+        "rs_hello_world",
+        py_fn!(py, rs_hello_world_py(name: &str)),
+    )?;
     Ok(())
 });
